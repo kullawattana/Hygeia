317c49da5666cae30c0fe6d4b7ab7199cb0c93dd
suttipong kullawattana <suttipong.kull@gmail.com>
2019-05-11 19:43:24 +0700

    Refactor

--- app/src/main/java/com/eng/chula/se/hygeia/api/Api.java
+++ app/src/main/java/com/eng/chula/se/hygeia/api/Api.java
@@ -1,20 +1,12 @@
 package com.eng.chula.se.hygeia.api;
 
 import com.eng.chula.se.hygeia.models.DefaultResponse;
-import com.eng.chula.se.hygeia.models.LoginResponse;
-import com.eng.chula.se.hygeia.models.UsersResponse;
-
-import java.math.BigDecimal;
 import java.util.Date;
 
 import retrofit2.Call;
-import retrofit2.http.DELETE;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
-import retrofit2.http.GET;
 import retrofit2.http.POST;
-import retrofit2.http.PUT;
-import retrofit2.http.Path;
 
 public interface Api {
 
